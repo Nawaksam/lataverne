78e524499ecdf006c5f02304ee8de6036ee27330
Nawaksam <sam.cardonnel@gmail.com>
2023-09-30 13:26:15 +0200

    updated dump no truncate

--- backend/database/dump-lataverne.sql
+++ backend/database/dump-lataverne.sql
@@ -28,14 +28,12 @@ CREATE TABLE `comment` (
   `user_id` int NOT NULL,
   `post_id` int NOT NULL,
   `creation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `like` int NOT NULL DEFAULT '0',
-  `dislike` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `comment_FK_user` (`user_id`),
   KEY `comment_FK_post` (`post_id`),
   CONSTRAINT `comment_FK_post` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `comment_FK_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,6 +42,7 @@ CREATE TABLE `comment` (
 
 LOCK TABLES `comment` WRITE;
 /*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+INSERT INTO `comment` VALUES (5,'C\'est bien vrai !! !!!!!!!',4,10,'2023-09-28 13:36:59'),(9,'C\'est bien vrai !!',2,10,'2023-09-28 15:06:09'),(14,'123\n',4,25,'2023-09-29 10:59:16');
 /*!40000 ALTER TABLE `comment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,7 +58,7 @@ CREATE TABLE `invitation` (
   `code` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `creation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,7 +67,7 @@ CREATE TABLE `invitation` (
 
 LOCK TABLES `invitation` WRITE;
 /*!40000 ALTER TABLE `invitation` DISABLE KEYS */;
-INSERT INTO `invitation` VALUES (1,'yololecode','2023-09-27 13:05:19');
+INSERT INTO `invitation` VALUES (1,'yololecode','2023-09-27 13:05:19'),(2,'lecode','2023-09-27 14:13:51');
 /*!40000 ALTER TABLE `invitation` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -85,12 +84,10 @@ CREATE TABLE `post` (
   `content` text COLLATE utf8mb4_general_ci NOT NULL,
   `user_id` int NOT NULL,
   `creation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `like` int NOT NULL DEFAULT '0',
-  `dislike` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `post_FK` (`user_id`),
   CONSTRAINT `post_FK` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -99,7 +96,39 @@ CREATE TABLE `post` (
 
 LOCK TABLES `post` WRITE;
 /*!40000 ALTER TABLE `post` DISABLE KEYS */;
+INSERT INTO `post` VALUES (1,'Hello','I\'am First',3,'2023-09-27 19:03:57'),(10,'La jungle','Nu comme un ver libre comme l\'air',4,'2023-09-28 11:31:11'),(25,'On teste','C\'est fracassé\n',2,'2023-09-28 17:15:03'),(28,'1234','4564567',7,'2023-09-29 15:15:42');
 /*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post_votes`
+--
+
+DROP TABLE IF EXISTS `post_votes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post_votes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `post_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `upvote` tinyint(1) NOT NULL DEFAULT '0',
+  `downvote` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `post_votes_FK` (`user_id`),
+  KEY `post_votes_FK_1` (`post_id`),
+  CONSTRAINT `post_votes_FK` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `post_votes_FK_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post_votes`
+--
+
+LOCK TABLES `post_votes` WRITE;
+/*!40000 ALTER TABLE `post_votes` DISABLE KEYS */;
+INSERT INTO `post_votes` VALUES (1,25,3,1,0),(3,25,2,0,1),(4,10,2,1,0),(6,25,4,1,0),(7,10,4,1,0);
+/*!40000 ALTER TABLE `post_votes` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -117,7 +146,7 @@ CREATE TABLE `user` (
   `register` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `roles` varchar(100) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '[user]',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -126,6 +155,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (2,'Samy','samy@samy.fr','$argon2id$v=19$m=65536,t=5,p=1$HjwY0HVivQCNnJrI85qAgg$RjOC2gBHI7r5RoR2mi7D9fLabMbfui1LV+TC6M4mujs','2023-09-27 16:47:35','[user,admin]'),(3,'Mario','mario@mario.fr','$argon2id$v=19$m=65536,t=5,p=1$qTOUXw2mmd3KbEg4v7WAMw$0Vt0Cmm9Cem9U9zZR0jLRnMwpyy+1kPT4PsoahwXWk0','2023-09-27 16:56:06','[user]'),(4,'Tarzan','tarzan@tarzan.fr','$argon2id$v=19$m=65536,t=5,p=1$L+QoqROnQsiz0d9JOVLyDw$MHSQzsQyIU+YZT1AlhfzSi5CpMo8qwYo3zrFhxzxexM','2023-09-27 17:08:24','[user]'),(6,'Tomy','tom@tom.fr','$argon2id$v=19$m=65536,t=5,p=1$3EL/X/I6GTdTwlCubCeVAg$u0uSBRHy2NaPp16n2OrF9InN7c6HnBjC214pOHXwsQA','2023-09-29 13:25:48','[user]'),(7,'Lapin','lapin@mignon.fr','$argon2id$v=19$m=65536,t=5,p=1$rtERxMBZOZbe4DIhOkBkoQ$rFj7daNpEinB7Q3/jFJdmLSrCG/A33/Eo4gfaO62Z6Q','2023-09-29 15:04:39','[user]');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -142,4 +172,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-27 14:01:09
+-- Dump completed on 2023-09-30 13:25:46
